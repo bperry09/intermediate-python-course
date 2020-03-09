50be39657612d0bf95cf4878c6e9956e47974df8
Bailey Perry <bailey.perry@optum.com>
2020-03-09 09:09:58 -0700

    Tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -9,7 +9,7 @@ def main():
 		if roll==1:
 			print(f'You rolled a {roll}! Critical Fail')
 		if roll==dice_size:
-			print(f'You rolled a {roll}! Critical Success')
+			print(f'You rolled a {roll}! Critical Success!')
 		else:
   			print(f'You rolled a {roll}')
 	print(f'You have rolled a total of {dice_sum}')
